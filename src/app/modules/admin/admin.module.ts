c5b1cfd6271e1bd9fbb20d5dbdb8d1f9b3bf32e5
KAISIN Guy <kaising@bpost.be>
2020-04-09 23:19:07 +0200

    merge 3

--- src/app/modules/admin/admin.module.ts
+++ src/app/modules/admin/admin.module.ts
@@ -39,7 +39,6 @@ import { SemainesByCategoryComponent } from './interclubs/semaines-by-category/s
     ListesdesforcesByCategoryComponent, 
     ListesdesforcesComponent, 
     DivisionsByCategoryComponent, SynchronisationComponent, CalendriersComponent, CalendriersByCategoryComponent, SemainesComponent, SemainesByCategoryComponent,
-    EquipesComponent, EquipesByCategoryComponent, RolesComponent, DomainsComponent, GroupesComponent, RoleComponent, GroupRoleComponent,
   ],
   imports: [
     CommonModule,
