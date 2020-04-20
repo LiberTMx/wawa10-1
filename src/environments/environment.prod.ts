bebda1f8794edc0e2011df65da426dd2027a1b41
KAISIN Guy <kaising@bpost.be>
2020-04-20 18:23:31 +0200

    update app version

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   apiUrl: 'http://site2-api.eoycwjp.cluster024.hosting.ovh.net/api',
   angular_version: '9.0.1',
-  app_version: ' 0.0.8 - 18/4/2020 0h30'
+  app_version: ' 0.0.9 - 20/4/2020 18h30'
 };
