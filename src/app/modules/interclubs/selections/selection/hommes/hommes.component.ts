1d45540e1b5fa40ada04ec571ab8a51b1c13bb33
KAISIN Guy <kaising@bpost.be>
2020-04-30 02:10:39 +0200

    small correction as menu is blocked - add idea

--- src/app/modules/interclubs/selections/selection/hommes/hommes.component.ts
+++ src/app/modules/interclubs/selections/selection/hommes/hommes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { InterclubsSemaineModel } from '../../model/interclubs-semaine.model';
+import { MatSelectChange } from '@angular/material/select';
 
 @Component({
   selector: 'app-interclubs-selections-hommes',
@@ -11,9 +12,21 @@ export class HommesComponent implements OnInit {
   @Input()
   semaines: Array<InterclubsSemaineModel>;
   
+  selectedSemaine: InterclubsSemaineModel=null;
+
+  loading=true;
+
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit(): void 
+  {
+
   }
 
+  
+  onChangeSemaine(event: MatSelectChange)
+  {
+    this.selectedSemaine = event.value;
+    console.log('semaine sélectionnée:', this.selectedSemaine);
+  }
 }
